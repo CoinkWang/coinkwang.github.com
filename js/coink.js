f9cb69188da5ac9068ee0ced075fb235d8d18a60
CoinkWang <coinkwang@qq.com>
2018-12-24 21:21:45 +0800

    Update js/coink.js

--- js/coink.js
+++ js/coink.js
@@ -1,24 +1,24 @@
 // console.log(" ________  ________  ___  ________   ___  __        \n|\\   ____\\|\\   __  \\|\\  \\|\\   ___  \\|\\  \\|\\  \\      \n\\ \\  \\___|\\ \\  \\|\\  \\ \\  \\ \\  \\\\ \\  \\ \\  \\/  /|_    \n \\ \\  \\    \\ \\  \\\\\\  \\ \\  \\ \\  \\\\ \\  \\ \\   ___  \\   \n  \\ \\  \\____\\ \\  \\\\\\  \\ \\  \\ \\  \\\\ \\  \\ \\  \\\\ \\  \\  \n   \\ \\_______\\ \\_______\\ \\__\\ \\__\\\\ \\__\\ \\__\\\\ \\__\\ \n    \\|_______|\\|_______|\\|__|\\|__| \\|__|\\|__| \\|__| \n");
 
-// console.log("圣诞老人告诉我：\n");
-// console.log("喜欢的东西依旧喜欢，但不一定要拥有\n");
-// console.log("害怕的东西同样害怕，但你得敢去面对\n");
-// console.log("我送你一份礼物，叫成熟\n");
-
 console.log("想你了\n");
 console.log("但我是个理性又矫情的人\n");
 console.log("知道你不会喜欢我\n");
-console.log("就只能咬着牙\n");
+console.log("就只会咬着牙\n");
 console.log("写下我不在乎\n");
 console.log("\n");
-console.log("最后，\n");
-console.log("我还是要把没来得及说的告白，偷偷留在这：\n");
+console.log("刚才圣诞老人告诉我：\n");
+console.log("喜欢的东西继续喜欢，不一定要拥有\n");
+console.log("害怕的东西同样害怕，但得敢去面对\n");
+console.log("送你一份礼物，叫成熟\n");
+console.log("\n");
+console.log("思来想去，\n");
+console.log("我还决定把没来得及说的表白，偷偷留在这：\n");
 console.log("傻子菲，圣诞了\n");
-console.log("祝你健康平安\n");
+console.log("你要健康平安\n");
 console.log("最好快乐幸福\n");
 console.log("\n");
 console.log("还有\n");
-console.log("要记得我呀\n");
+console.log("要记得我\n");
 
 // 圣诞装饰
 setInterval(function () { window.location.hash = "🎄"; }, 1000);
